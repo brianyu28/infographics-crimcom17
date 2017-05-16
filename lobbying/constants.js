3b7ea2064580b8da8fcd85849f6ee22856538c1a
Brian Yu <brianyu28@gmail.com>
2017-05-16 16:34:25 -0400

    Finish draft of lobbying

--- lobbying/constants.js
+++ lobbying/constants.js
@@ -1,9 +1,104 @@
-var data = [];
+var data = [
+    {
+        "name": "Sen. Al Franken",
+        "detail": "(D-MN)",
+        "image": "img/franken.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Maggie Hassan",
+        "detail": "(D-NH)",
+        "image": "img/hassan.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Susan Collins",
+        "detail": "(R-ME)",
+        "image": "img/collins.png",
+        "group": 2
+    },
+    {
+        "name": "Sen. Patrick Leahy",
+        "detail": "(D-VT)",
+        "image": "img/leahy.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Jack Reed",
+        "detail": "(D-RI)",
+        "image": "img/reed.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Mike Crapo",
+        "detail": "(R-ID)",
+        "image": "img/crapo.png",
+        "group": 2
+    },
+    {
+        "name": "Sen. Chuck Schumer",
+        "detail": "(D-NY)",
+        "image": "img/schumer.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Orrin Hatch",
+        "detail": "(R-UT)",
+        "image": "img/hatch.png",
+        "group": 2
+    },
+    {
+        "name": "Sen. Mitch McConnell",
+        "detail": "(R-KY)",
+        "image": "img/mcconnell.png",
+        "group": 2
+    },
+    {
+        "name": "Sen. Chris Van Hollen",
+        "detail": "(D-MD)",
+        "image": "img/vanhollen.png",
+        "group": 1
+    },
+    {
+        "name": "Sen. Ben Sasse",
+        "detail": "(R-NE)",
+        "image": "img/sasse.png",
+        "group": 2
+    },
+    {
+        "name": "Rep. Kevin Brady",
+        "detail": "(R-TX 8th District)",
+        "image": "img/brady.png",
+        "group": 2
+    },
+    {
+        "name": "Rep. Ron Kind",
+        "detail": "(D-WI 3rd District)",
+        "image": "img/kind.png",
+        "group": 1
+    },
+    {
+        "name": "Rep. Paul Ryan",
+        "detail": "(R-WI 1st District)",
+        "image": "img/ryan.png",
+        "group": 2
+    },
+    {
+        "name": "Rep. Scott Taylor",
+        "detail": "(R-VA 2nd District)",
+        "image": "img/taylor.png",
+        "group": 2
+    },
+];
 
 var graphicWidth = 600;
-var padding = 10;
+var padding = 15;
+var glowSize = 15;
 
-var textSize = 14;
+var textSize = 12;
+
+var imageSize = 120;
+var imagesPerRow = 3;
 
 var crimson = '#a82931';
 var blue1 = '#004e6a';
@@ -15,7 +110,7 @@ var white = '#ffffff';
 var black = '#000000';
 
 var timelineColor = black;
-var textColor = black;
+var textColor = white;
 var buttonColor = blue2;
 
 var mainFont = '"Merriweather", serif';
--- lobbying/lobbying.js
+++ lobbying/lobbying.js
@@ -5,14 +5,159 @@ window.onload = function() {
 var originX = 0;
 var originY = 0;
 
+var rows; // number of rows
+var colLength;
+
 function render() {
     var canvas = d3.select('#lobbying-graphic')
         .style('width', px(graphicWidth));
+    makeHeadline(canvas);
 
+    rows = Math.ceil(data.length / imagesPerRow);
+    var requiredHeight = (rows + 1) * (2 * padding) + (rows * imageSize);
     var svg = canvas.append('svg')
         .style('width', px(graphicWidth))
-        .style('height', px(graphicWidth))
+        .style('height', px(requiredHeight))
         .style('margin-left', px(padding))
         .style('margin-right', px(padding));
+   
+    colLength = graphicWidth / imagesPerRow;
+    var endPadding = colLength - imageSize;
+    var baseX = originX + colLength - (endPadding / 2) - imageSize;
+    var currentX = baseX;
+    var currentY = originY + 2 * padding;
+
+    for (var i = 0; i < data.length; i++) {
+    
+        // reset x and y to a new row if we need to
+        if (i % imagesPerRow === 0) {
+            currentX = baseX;
+            if (i !== 0)
+                currentY += imageSize + 2 * padding;
+        }
+    
+        drawPoint(svg, currentX, currentY, i);
+        currentX += colLength;
+    }
 }
 
+function makeHeadline(canvas) {
+    // show primary headline 
+    var headline = canvas.append('div')
+        .style('margin-left', px(padding))
+        .style('margin-right', px(padding))
+        .style('text-align', 'center')
+        .style('font-size', px(28))
+        .style('font-family', mainFont)
+        .text('Lawmakers University President Drew G. Faust has met with recently');
+
+}
+
+function drawPoint(svg, x, y, i) {
+    var image = svg.append('image')
+        .attr('xlink:href', data[i]['image'])
+        .attr('x', x)
+        .attr('y', y)
+        .attr('width', imageSize)
+        .attr('height', imageSize)
+        .on('mouseover', function() {
+            showDetail(svg, i, x, y);
+        })
+        .on('mouseout', function() {
+            hideDetail();
+        });
+    animate(svg, image, data[i]['group']);
+}
+
+details = [];
+
+function showDetail(svg, i, x, y) {
+
+    group = parseInt(data[i]['group']);
+
+    hideDetail();
+    
+    var rectWidth = 150;
+    var rectHeight = 40;
+
+    var rectX;
+    var column = Math.floor(x / (colLength + 2 * padding));
+    if (column < (imagesPerRow - 1) / 2)
+        rectX = x + 0.75 * imageSize;
+    else if (column > (imagesPerRow - 1) / 2)
+        rectX = x + 0.25 * imageSize - rectWidth;
+    else
+        rectX = x + 0.5 * imageSize - (rectWidth / 2);
+
+    var rectY;
+    var row = Math.floor(i / imagesPerRow);
+    if (row > (rows - 1) / 2)
+        rectY = y - 0.25 * imageSize;
+    else if (row < (rows - 1) / 2)
+        rectY = y + 0.75 * imageSize;
+    else
+        rectY = y;
+
+    rectX = x + (imageSize / 2) - (rectWidth / 2);
+    rectY = y + (imageSize) - (rectHeight / 2);
+    var rect = svg.append('rect')
+        .attr('x', rectX)
+        .attr('y', rectY)
+        .attr('width', rectWidth)
+        .attr('height', rectHeight)
+        .attr('fill', colorForGroup(group));
+    details.push(rect);
+
+    var name = svg.append('text')
+        .attr('x', rectX + (rectWidth / 2))
+        .attr('y', rectY + (2 * rectHeight / 5))
+        .attr('width', rectWidth)
+        .attr('height', rectHeight)
+        .style('font-family', mainFont)
+        .style('font-size', textSize)
+        .style('text-anchor', 'middle')
+        .style('fill', textColor)
+        .text(data[i]['name']);
+    details.push(name);
+
+    var detail = svg.append('text')
+        .attr('x', rectX + (rectWidth / 2))
+        .attr('y', rectY + (4 * rectHeight / 5))
+        .attr('width', rectWidth)
+        .attr('height', rectHeight)
+        .style('font-family', mainFont)
+        .style('font-size', textSize)
+        .style('text-anchor', 'middle')
+        .style('fill', textColor)
+        .text(data[i]['detail']);
+    details.push(detail);
+};
+
+function hideDetail() {
+    for (var i = 0; i < details.length; i++) {
+        details[i].remove();
+    }
+    details = [];
+}
+
+function colorForGroup(i) {
+    if (i == 1)
+        return blue1;
+    else if (i == 2)
+        return crimson;
+    else
+        return green;
+}
+
+function animate(svg, point, group) {
+    for (var i = 0; i < glowSize; i++) {
+        var circleSize = (imageSize / 2) + i;
+        var glow = svg.insert('circle', 'image')
+            .attr('cx', parseFloat(point.attr('x')) + parseFloat(point.attr('width'))/2)
+            .attr('cy', parseFloat(point.attr('y')) + parseFloat(point.attr('height'))/2)
+            .attr('r', circleSize)
+            .style('fill', 'none')
+            .style('stroke', colorForGroup(group))
+            .style('stroke-opacity', 1 - (i / glowSize));
+    }
+}
